ac839097b22169eb7694b796fd4e8f6bfc30391d
Piet Geursen <pietgeursen@gmail.com>
2015-11-23 19:14:25 +1300

    Mikeys example test.

--- test/overPopulated.js
+++ test/overPopulated.js
@@ -1,12 +1,28 @@
 var test = require('tape');
 var overPopulated = require('../overPopulated')
 
-test('#should return true with cell count > 3', function (t) {
-  t.plan(1)
-  t.true(overPopulated(4))
-})
+test('overPopulated normal cases', function (t) {
+  var expecteds = {
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+    4: true,
+    5: true,
+    6: true,
+    7: true,
+    8: true
+  }
 
-test('#should return true with cell count < 4', function (t) {
-  t.plan(1)
-  t.false(overPopulated(3))
+  Object.keys(expecteds).forEach(function (input) {
+    var expected = expecteds[input]
+    var actual = overPopulated(input)
+
+    var message = "cell count " + input + " is " +
+      (expected ? "overpopulated" : "not overpopulated")
+
+    t.equal(actual, expected, message)
+  })
+
+  t.end()
 })
