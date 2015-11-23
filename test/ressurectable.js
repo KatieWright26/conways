59ac4bf4ee7be7afdc5c22170ca3791d21e81a74
Piet Geursen <pietgeursen@gmail.com>
2015-11-24 08:05:23 +1300

    Tape test

--- test/ressurectable.js
+++ test/ressurectable.js
@@ -1,11 +1,28 @@
-var expect = require('expect.js')
+var test = require('tape')
 var ressurectable = require('../ressurectable')
 
-describe('#ressurectable', function () {
-  it('should return true with cell count == 3', function () {
-    expect(ressurectable(3)).to.be(true)
+test('ressurectable normal cases', function (t) {
+  var expecteds = {
+    0: false,
+    1: false,
+    2: false,
+    3: true,
+    4: false,
+    5: false,
+    6: false,
+    7: false,
+    8: false
+  }
+
+  Object.keys(expecteds).forEach(function (input) {
+    var expected = expecteds[input]
+    var actual = ressurectable(parseInt(input, 10))
+
+    var message = "cell count " + input + " is " +
+      (expected ? "ressurectable" : "not ressurectable")
+
+    t.equal(actual, expected, message)
   })
-  it('should return false with cell count != 3', function () {
-    expect(ressurectable(2)).to.be(false)
-  })
+
+  t.end()
 })
