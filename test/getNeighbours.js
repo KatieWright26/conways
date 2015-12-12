de73b72becaa5a025b651d5065c6e44e2663ddb6
Piet Geursen <pietgeursen@gmail.com>
2015-12-12 17:46:11 +1300

    Improve neighbour test

--- test/getNeighbours.js
+++ test/getNeighbours.js
@@ -11,3 +11,18 @@ test('getNeighbours in corner', function (t) {
   t.equal(neighbours.reduce(function (sum, val) { return sum + val }, 0), 19)
   t.end()
 })
+test('getNeighbours in middle', function (t) {
+  var board = createBoard(10)
+  var total = 0
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (i === 1 && j === 1) continue
+      var rando = Math.random()
+      total += rando
+      board[i][j] = rando
+    }
+  }
+  var neighbours = getNeighbours(1, 1, board)
+  t.equal(neighbours.reduce(function (sum, val) { return sum + val }, 0), total)
+  t.end()
+})
