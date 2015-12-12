7b6efa858018d45d3a95cfb600784ecf8d5e4ec9
Piet Geursen <pietgeursen@gmail.com>
2015-12-12 17:10:06 +1300

    Better test for corner neighbours

--- test/getNeighbours.js
+++ test/getNeighbours.js
@@ -2,9 +2,12 @@ var test = require('tape')
 var getNeighbours = require('../getNeighbours')
 var createBoard = require('../conway.js').createBoard
 
-test('getNeighbours', function (t) {
+test('getNeighbours in corner', function (t) {
   var board = createBoard(10)
-  t.equal(getNeighbours(2, 2, board).length, 8)
-  t.equal(getNeighbours(0, 0, board).length, 3)
+  board[0][1] = 3
+  board[1][0] = 5
+  board[1][1] = 11
+  var neighbours = getNeighbours(0, 0, board)
+  t.equal(neighbours.reduce(function (sum, val) { return sum + val }, 0), 19)
   t.end()
 })
