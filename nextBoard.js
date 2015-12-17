f10ee191641163076e302221c6560c6d105ed5ee
Piet Geursen <pietgeursen@gmail.com>
2015-12-18 10:42:23 +1300

    Refactor nextBoard. Soooo nice.

--- nextBoard.js
+++ nextBoard.js
@@ -1,18 +1,13 @@
 var countAliveNeighbours = require('./countAliveNeighbours')
-var createBoard = require('./createBoard')
 var nextCellState = require('./nextCellState')
 
 function nextBoard (currentBoard) {
-  var nextBoard = createBoard(currentBoard.count())
-  for (var i = 0; i < currentBoard.count(); i++) {
-    for (var j = 0; j < currentBoard.count(); j++) {
-      var cell = currentBoard.getIn([i, j])
-      var neighbourCount = countAliveNeighbours(i, j, currentBoard)
-      var nextCell = nextCellState(cell, neighbourCount)
-      nextBoard = nextBoard.setIn([i, j], nextCell)
-    }
-  }
-  return nextBoard
+  return currentBoard.map((row, rowIndex) => {
+    return row.map((cell, cellIndex) => {
+      var neighbourCount = countAliveNeighbours(rowIndex, cellIndex, currentBoard)
+      return nextCellState(cell, neighbourCount)
+    })
+  })
 }
 
 module.exports = nextBoard
