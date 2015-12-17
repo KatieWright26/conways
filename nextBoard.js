98ef4448a19e6c75a277555bc4022ce28e2e1865
Piet Geursen <pietgeursen@gmail.com>
2015-12-18 11:25:54 +1300

    remove es6 code

--- nextBoard.js
+++ nextBoard.js
@@ -2,8 +2,8 @@ var countAliveNeighbours = require('./countAliveNeighbours')
 var nextCellState = require('./nextCellState')
 
 function nextBoard (currentBoard) {
-  return currentBoard.map((row, rowIndex) => {
-    return row.map((cell, cellIndex) => {
+  return currentBoard.map(function (row, rowIndex) {
+    return row.map(function (cell, cellIndex) {
       var neighbourCount = countAliveNeighbours(rowIndex, cellIndex, currentBoard)
       return nextCellState(cell, neighbourCount)
     })
