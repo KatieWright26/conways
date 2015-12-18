1accf97f3b0b4b530324f7db0bde321da75c5634
Piet Geursen <pietgeursen@gmail.com>
2015-12-18 15:53:32 +1300

    Improve test

--- test/createBoard.js
+++ test/createBoard.js
@@ -6,5 +6,5 @@ test('createBoard', function (t) {
   var board = createBoard(length)
   t.plan(2)
   t.equal(board.count(), length, 'dimension 1 of the array is the length passed into the constructor')
-  t.equal(board.count(), length, 'dimension 2 of the array is the length passed into the constructor')
+  t.equal(board.get(0).count(), length, 'dimension 2 of the array is the length passed into the constructor')
 })
